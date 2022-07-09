8d224a331e3b8fbf35a3bce62b06cf32aef3432a
Djobbo <hi@djobbo.com>
2022-07-09 08:19:54 +0200

    change st font size

--- .config/st/config.h
+++ .config/st/config.h
@@ -5,7 +5,7 @@
  *
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
-static char *font = "FiraCode Nerd Font:pixelsize=16:antialias=true:autohint=true";
+static char *font = "FiraCode Nerd Font:pixelsize=14:antialias=true:autohint=true";
 static int borderpx = 8;
 
 /*
