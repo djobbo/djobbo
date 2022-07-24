57bb6f37605f95c05bd06cae3da31cbee1bd34ff
Djobbo <hi@djobbo.com>
2022-07-24 15:12:57 +0200

    update better discord install script

--- _install.sh
+++ _install.sh
@@ -13,6 +13,4 @@ ln -s $AKI_PATH/theme/aki.zsh-theme ~/.oh-my-zsh/custom/themes/aki.zsh-theme || 
 ln -s $AKI_PATH/.config/i3/config ~/.config/i3/config || true
 ln -s $AKI_PATH/.config/rofi ~/.config/rofi || true
 ln -s $AKI_PATH/.config/eww ~/.config || true
-ln -s $AKI_PATH/.config/BetterDiscord ~/.config || true
-ln -s $AKI_PATH/.config/discord/settings.json ~/.config/discord || true
 ln -s $AKI_PATH/.config/qtile ~/.config || true
--- install-scripts/theme/better-discord.sh
+++ install-scripts/theme/better-discord.sh
@@ -1,5 +1,7 @@
 #! /bin/bash
 
+sudo apt-get remove discord -y || true
+
 mkdir -p ~/.aki/.tmp
 
 wget -O ~/.aki/.tmp/discord.deb "https://discord.com/api/download?platform=linux&format=deb"
@@ -8,15 +10,27 @@ sudo dpkg -i ~/.aki/.tmp/discord.deb
 
 rm ~/.aki/.tmp/discord.deb
 
+mkdir -p ~/.config/discord
+ln -s $AKI_PATH/.config/discord/settings.json ~/.config/discord || true
+
+# Launch discord 1 time before injection for it to be successful
+nohup discord &
+discord_pid=$!
+sleep 5s
+kill -KILL $discord_pid
+
 git clone https://github.com/BetterDiscord/BetterDiscord.git ~/.aki/.tmp/BetterDiscord
 
 cd ~/.aki/.tmp/BetterDiscord
 
-# build works on node v14.x
-nvm use 14
+# build works on node v16.x
+nvm install 16
+nvm use 16
 
 npm install
 
 npm run build
 
 npm run inject
+
+ln -s $AKI_PATH/.config/BetterDiscord ~/.config || true
