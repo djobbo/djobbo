0d109661e195dda77e27b0d237b80f0b7a772a4f
Djobbo <hi@djobbo.com>
2022-07-24 14:07:06 +0200

    move to kitty

--- install-scripts/theme/_install.sh
+++ install-scripts/theme/_install.sh
@@ -29,3 +29,4 @@ sudo apt-get install -y rofi
 curl -L https://sw.kovidgoyal.net/kitty/installer.sh | sh /dev/stdin
 
 ln -s ~/.local/kitty.app/bin/kitty ~/.local/bin || true
+ln -s $AKI_PATH/.config/kitty ~/.config || true
