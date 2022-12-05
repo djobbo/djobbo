0da6b3e92539cdcf4079a5ab09bc1b8222570b33
Djobbo Maïga <4998293+AlfieGoldson@users.noreply.github.com>
2022-12-05 15:14:04 +0100

    fix astro build

--- portfolio/generateResume.spec.ts
+++ portfolio/generateResume.spec.ts
@@ -36,7 +36,7 @@ test("generate resume", async ({ browser }) => {
     expect(data).toBeTruthy()
 })
 
-test("generates github readme banner", async ({ browser }) => {
+test("generate github readme banner", async ({ browser }) => {
     const browserContext = await browser.newContext({
         viewport: {
             width: 960,
