d8ff76aa1f654a385c768cd1466a571ae1d6b63d
Djobbo Maïga <4998293+AlfieGoldson@users.noreply.github.com>
2022-12-09 01:42:32 +0100

    moar optimization

--- portfolio/astro.config.mjs
+++ portfolio/astro.config.mjs
@@ -2,15 +2,22 @@ import { defineConfig } from "astro/config"
 import tailwind from "@astrojs/tailwind"
 import react from "@astrojs/react"
 import image from "@astrojs/image"
+import partytown from "@astrojs/partytown"
+import sitemap from "@astrojs/sitemap"
+import compress from "astro-compress"
 
 // https://astro.build/config
 export default defineConfig({
+    site: "https://djobbo.com",
     integrations: [
         tailwind(),
         react(),
         image({
             serviceEntryPoint: "@astrojs/image/sharp",
         }),
+        partytown(),
+        sitemap(),
+        compress(),
     ],
     vite: {
         ssr: {
