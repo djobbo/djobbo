ea676dde4e538b91ca84deca37fb8bfc0327306c
Djobbo Maïga <4998293+AlfieGoldson@users.noreply.github.com>
2022-12-09 01:32:21 +0100

    image optimization

--- portfolio/astro.config.mjs
+++ portfolio/astro.config.mjs
@@ -1,10 +1,17 @@
 import { defineConfig } from "astro/config"
 import tailwind from "@astrojs/tailwind"
 import react from "@astrojs/react"
+import image from "@astrojs/image"
 
 // https://astro.build/config
 export default defineConfig({
-    integrations: [tailwind(), react()],
+    integrations: [
+        tailwind(),
+        react(),
+        image({
+            serviceEntryPoint: "@astrojs/image/sharp",
+        }),
+    ],
     vite: {
         ssr: {
             noExternal: ["@radix-ui/react-dropdown-menu", "usehooks-ts"],
